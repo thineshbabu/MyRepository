88c138a9cc8d8cb096656aa9ad27f6acedceff5e
TAYYASA <TAYYASA@centurylink.com>
2016-08-03 19:13:16 +0530

    Added arraylist test class

---
+++ SampleWebApp/src/main/java/ArrayListTest.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ArrayListTest {
+
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<String>();
+		list.add("Thinesh");
+		list.add("Sharme");
+		list.add("Iniyavan");
+		for(String s: list)
+		{
+			System.out.println("Family Names = " + s);
+		}
+	}
+
+}
